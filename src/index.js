5a3ab5b7b02dc85efa273a3ba1d2b2c9473709b8
Biinod81 <thomas.ly@iologo.io>
2022-04-16 17:36:52 +0200

    firebase setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import { GlobalStyles } from './global-styles'
 import 'normalize.css'
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{firebase}} >
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>
 );
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,3 +1,16 @@
-import Firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import Firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const config = {
+  apiKey: "AIzaSyBrkf4Dw3PvSNYTzmEYbAnBPXbEbAwPpZY",
+  authDomain: "netflix-clone-8a06c.firebaseapp.com",
+  projectId: "netflix-clone-8a06c",
+  storageBucket: "netflix-clone-8a06c.appspot.com",
+  messagingSenderId: "32417568121",
+  appId: "1:32417568121:web:14b51b0923aa2133cbcba8"
+}
+
+const firebase = Firebase.initializeApp(config)
+
+export { firebase }
