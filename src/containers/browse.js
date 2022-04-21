5af2ba3cbfdd0c2edc005717302dcec370c2a6b8
Biinod81 <thomas.ly@iologo.io>
2022-04-21 22:20:56 +0200

    removed unecessary files and folders

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -5,15 +5,10 @@ import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "./footer";
 import {
   Loading,
-  Header,
-  Card,
-  Player,
   Row,
   Banner,
   Navbar,
 } from "../components";
-import * as ROUTES from "../constants/routes";
-import logo from "../logo.svg";
 import requests from "../hooks/requests";
 import "./browse.css";
 
@@ -52,50 +47,8 @@ export function BrowseContainer({ slides }) {
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
-      {/* <Header>
-        <Header.Frame>
-          <Header.Group>
-            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-            <Header.TextLink
-              active={category === "series" ? "true" : "false"}
-              onClick={() => setCategory("series")}
-            >
-              Series
-            </Header.TextLink>
-            <Header.TextLink
-              active={category === "films" ? "true" : "false"}
-              onClick={() => setCategory("films")}
-            >
-              Films
-            </Header.TextLink>
-          </Header.Group>
-          <Header.Group>
-            <Header.Search
-              searchTerm={searchTerm}
-              setSearchTerm={setSearchTerm}
-            />
-            <Header.Profile>
-              <Header.Picture src={user.photoURL} />
-              <Header.Dropdown>
-                <Header.Group>
-                  <Header.Picture src={user.photoURL} />
-                  <Header.TextLink>{user.displayName}</Header.TextLink>
-                </Header.Group>
-                <Header.Group>
-                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
-                    Sign out
-                  </Header.TextLink>
-                </Header.Group>
-              </Header.Dropdown>
-            </Header.Profile>
-          </Header.Group>
-        </Header.Frame>
-      </Header> */}
       <div className="browse">
         <Navbar />
-        {/* <Header.TextLink onClick={() => firebase.auth().signOut()}>
-          Sign out
-        </Header.TextLink> */}
         <Banner />
 
         {/* Rows */}
